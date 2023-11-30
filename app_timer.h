e82d1888c69385819a1417a6ae7095d576ae51cc
Micxxxman <lamwingho007@gmail.com>
2023-11-30 15:20:08 +0800

    ff

---
+++ app_timer.h
@@ -0,0 +1,61 @@
+// app_timer
+
+#ifndef APP_TIMER 
+#define APP_TIMER
+
+
+#include <iostream>
+#include <functional>
+#include <thread>
+#include <chrono>
+#include "app_timer.h"
+
+namespace N
+{
+    class app_timer {
+public:
+    app_timer() : isRunning(false) {}
+
+    // Set the callback event handler
+    void setCallback(const std::function<void()>& callback) {
+        this->callback = callback;
+    }
+
+    // Start the timer with the specified interval in milliseconds
+    void start(int interval) {
+        if (isRunning) {
+            return;
+        }
+
+        isRunning = true;
+        std::thread timerThread([this, interval]() {
+            while (isRunning) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+                if (callback) {
+                    callback();
+                }
+            }
+        });
+        timerThread.detach();
+    }
+
+    // Stop the timer
+    void stop() {
+        isRunning = false;
+    }
+
+private:
+    std::function<void()> callback;
+    bool isRunning;
+};
+} // namespace N
+
+
+
+
+
+#endif 
+// class app_timer{
+
+
+// }
